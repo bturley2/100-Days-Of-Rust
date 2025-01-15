3073395b50240f7f2a5ebd05a2b2077fb28f119c
Bryce Turley <turleyb2@gmail.com>
2025-01-15 12:32:10 -0800

    determined pattern matching wasn't appropriate for finding nemo

--- Week-01/Day-02_Finding-Nemo/finding_nemo/src/main.rs
+++ Week-01/Day-02_Finding-Nemo/finding_nemo/src/main.rs
@@ -7,6 +7,24 @@ fn main() {
     find_nemo("There was no fishy...")
 }
 
+/*
+fn find_nemo2(text: &str) {
+    let nemo = "Nemo";
+    println!("Searching: '{text}'");
+
+    // uses pattern matching and loops through iterator differently
+    let mut split_pieces: Split<&str> = text.split(' ');
+
+    loop {
+        match split_pieces.next() {
+            Some(nemo) => println!(""),
+            Some(s) => println!(""),
+            None => println!(""),
+        }
+    }
+}
+*/
+
 fn find_nemo(text: &str) {
     let nemo = "Nemo";
     println!("Searching: '{text}'");
